306f689a2676d2fe88afac77eedd684a3901290e
Ajala Ayodotun <dotunajala@gmail.com>
2018-04-13 16:40:15 +0100

    initial setup

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import Header from './components/Header/Header';
+
+class App extends Component {
+
+
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
